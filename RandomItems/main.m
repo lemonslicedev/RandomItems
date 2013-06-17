e43215d67e2fca39888209e6392aba625eafb601
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro-2.local>
2013-06-17 09:32:12 -0400

    Cleared main.m.

--- RandomItems/main.m
+++ RandomItems/main.m
@@ -14,33 +14,6 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        [items addObject:@"One"];
-        [items addObject:@"Two"];
-        [items addObject:@"Three"];
-        
-        //Adding this object at index 0 will shift all the elements
-        [items insertObject:@"Zero" atIndex:0];
-        
-        //You cannot add nil to an array, if you need to add holes use object NSNull
-        [items addObject:[NSNull null]];
-        //Adding object at index two will replace string "Two" with nothing
-        [items replaceObjectAtIndex:2 withObject:[NSNull null]];
-        
-        for (int i = 0; i < [items count]; i++) {
-            NSLog(@"%@", [items objectAtIndex:i]);
-        }
-        
-//        int numberOfObjects = [items count];
-//        
-//        NSLog(@"%d", numberOfObjects);
-        
-        //Retrieves pointer to the object at index 3.
-        NSString *obj = [items objectAtIndex: 3];
-        //Will print the description of the object at index 3, which is simply string "Three".
-        NSLog(@"%@", obj);
-        
-        //Will print the description of the array items. Will print out the name of the array and the elements.
-        NSLog(@"%@", items);
         items = nil;
     }
     return 0;
